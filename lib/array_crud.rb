990391af76163fbb8452fddbd66577bb0ab41edb
Will Drougas <wdrougas@gmail.com>
2019-08-07 14:00:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["Declan", "Justin"]
-  add_element_to_end_of_array.push("Mike")
+  add_element_to_end_of_array << "Mike"
 end
 
 def add_element_to_start_of_array(array, element)
